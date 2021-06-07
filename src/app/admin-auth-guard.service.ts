9e5f8bd17bdb789a134f435ef9a48b9735048f4e
Faysal Ibn Mizan <ovitahsan@gmail.com>
2021-06-08 00:43:40 +0200

    Project Authentication and Authorization:
    - Showing or Hiding the admin links

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -1,4 +1,4 @@
-import { AppUser } from './models/appUser';
+
 import { UsersService } from './user.service';
 import { AuthService } from './auth.service';
 import { Observable } from 'rxjs';
@@ -8,7 +8,7 @@ import {
   RouterStateSnapshot,
 } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { switchMap, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -23,12 +23,6 @@ export class AdminAuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> {
-    return this.authService.user$.pipe(
-      switchMap((user) =>
-        this.usersService
-          .get(user.uid)
-          .pipe(map((appUser) => (appUser as AppUser).isAdmin))
-      )
-    );
+    return this.authService.AppUser$.pipe(map((appUser) => appUser.isAdmin));
   }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,12 @@
+import { UsersService } from './user.service';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { AppUser } from './models/appUser';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +15,10 @@ export class AuthService {
   user$: Observable<firebase.User>;
 
   constructor(
+    private usersService: UsersService,
     private angularFireAuth: AngularFireAuth,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {
     this.user$ = this.angularFireAuth.authState;
   }
@@ -33,6 +39,16 @@ export class AuthService {
   // }
 
   logout(): void {
-    this.angularFireAuth.signOut();
+    this.angularFireAuth.signOut().then(() => this.router.navigate(['/']));
+  }
+
+  get AppUser$(): Observable<AppUser> {
+    return this.user$.pipe(
+      switchMap((user) => {
+        if (user) return this.usersService.get(user.uid);
+
+        return of(null);
+      })
+    );
   }
 }
