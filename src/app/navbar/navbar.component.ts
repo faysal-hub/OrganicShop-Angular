de2e3ef588a78ca551f21d1d571d7facc6aa5e5e
Faysal Ibn Mizan <ovitahsan@gmail.com>
2021-12-30 05:17:00 +0100

    Project Shopping Cart: - Displaying the Number of Shopping Cart Items in Navbar.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,18 +1,31 @@
 import { Observable } from 'rxjs';
 import { AppUser } from './../models/appUser';
 import { AuthService } from './../auth.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   appUser$: Observable<AppUser>;
+  cartLinesCount: number;
 
-  constructor(private authService: AuthService) {
-    this.appUser$ = authService.AppUser$;
+  constructor(
+    private authService: AuthService,
+    private cartService: CartService
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    this.appUser$ = this.authService.AppUser$;
+
+    (await this.cartService.getCart()).valueChanges().subscribe((c) => {
+      this.cartLinesCount = 0;
+      for (let productId in c.cartLines)
+        this.cartLinesCount += c.cartLines[productId].quantity;
+    });
   }
 
   logout() {
