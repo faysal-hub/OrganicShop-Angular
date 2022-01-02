0800862aea7a39e160e6e2eba9df987ba9825cb1
Faysal Ibn Mizan <ovitahsan@gmail.com>
2022-01-02 14:19:08 +0100

    Project Check Out Module: - Building a Shipping Form.

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgForm } from '@angular/forms';
+import { Shipping } from './../models/shipping';
+
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+  shipping: Shipping = {} as Shipping;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  placeOrder(): void {
+    console.log(this.shipping);
   }
-
+  
 }
---
+++ src/app/models/shipping.ts
@@ -0,0 +1,9 @@
+export interface Shipping {
+  name: string;
+  address1: string;
+  address2: string;
+  cityName: string;
+  stateName: string;
+  zipNumber: number;
+  gridCheck: true;
+}
