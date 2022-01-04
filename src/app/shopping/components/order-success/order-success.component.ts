e1f86161b2581eff708f9278869c08915467784c
Faysal Ibn Mizan <ovitahsan@gmail.com>
2022-01-04 17:16:16 +0100

    Project Modularization and Final Improvements: - rewrite the order-success page

--- src/app/shopping/components/order-success/order-success.component.ts
+++ src/app/shopping/components/order-success/order-success.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-success',
   templateUrl: './order-success.component.html',
-  styleUrls: ['./order-success.component.css']
+  styleUrls: ['./order-success.component.css'],
 })
-export class OrderSuccessComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class OrderSuccessComponent {
+  constructor(route: Router) {
+    setTimeout(() => {
+      route.navigate(['/']);
+    }, 5000);
   }
-
 }
